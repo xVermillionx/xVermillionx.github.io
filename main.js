63c0d36855201c439ba2e5acb37f61a3b71f64b2
xVermillionx <xVermillionx@notvalid>
2022-11-12 22:37:38 +0100

    Fetch Public repo info on page load

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
+const star = '&#11088;'
+const eye = '&#128065;'
+const noemoji = '&#xFE0E;'
 
+window.fetch("https://api.github.com/users/xVermillionx/repos", {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    // let newdata = data.map((e) => [e.name, e.html_url, e.stargazers_count, e.watchers_count])
+    let pick = ['name', 'html_url', 'stargazers_count', 'watchers_count'];
+    let newdata = data.map((e) => Object.fromEntries(Object.entries(e).filter(r => pick.includes(r[0]))))
+    for(e of newdata) {
+      let div = document.createElement('div');
+      div.className = "tile";
+      let a = document.createElement("a");
+      a.href = e.html_url;
+      a.innerText = e.name;
+      b = a.cloneNode()
+      c = a.cloneNode()
+      b.innerHTML = `${e.stargazers_count}`;
+      c.innerHTML = `${e.watchers_count}`;
+      div.appendChild(a);
+      div.appendChild(b);
+      div.appendChild(c);
+      // let sections = document.getElementsByTagName('section');
+      // sections[sections.length-1].appendChild(div);
+      let repolist = document.getElementById('repos');
+      let li = document.createElement('li');
+      li.appendChild(div);
+      repolist.appendChild(li);
+    }
+    console.log('Success: ', newdata)
+  })
+  .catch((err) => console.log('Error: ', err))
